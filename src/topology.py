910ea9c5ba74b2674111c243b7595f92c89c1f59
samMM-cs <morrealesamuele3@gmail.com>
2025-12-29 19:11:25 +0100

    initial implementation

---
+++ src/topology.py
@@ -0,0 +1,62 @@
+from mininet.net import Mininet, CLI
+from mininet.link import TCLink
+from mininet.topo import Topo
+from mininet.node import Switch
+
+
+class Topology(Topo):
+    def __init__(self):
+        super().__init__()
+        # routers will be ryu switches
+        R1: Switch = self.addSwitch("R1")
+        R2: Switch = self.addSwitch("R2")
+        R3: Switch = self.addSwitch("R3")
+        R4: Switch = self.addSwitch("R4")
+        R5: Switch = self.addSwitch("R5")
+        R6: Switch = self.addSwitch("R6")
+
+        # router-router connections
+        self.addLink(R1, R3, cls=TCLink, bw=500, delay="10ms")
+        self.addLink(R3, R5, cls=TCLink, bw=500, delay="10ms")
+        self.addLink(R5, R6, cls=TCLink, bw=1000, delay="10ms")
+        self.addLink(R3, R4, cls=TCLink, bw=1000, delay="10ms")
+        self.addLink(R4, R2, cls=TCLink, bw=500, delay="10ms")
+
+        # subnet behind R1
+        H1 = self.addHost("H1", ip="10.0.0.2/24")
+        H2 = self.addHost("H2", ip="10.0.0.3/24")
+        self.addLink(R1, H1, cls=TCLink, bw=100, delay="1ms")
+        self.addLink(R1, H2, cls=TCLink, bw=100, delay="1ms")
+
+        # subnet behind R3
+        H3 = self.addHost("H3", ip="10.1.0.2/24")
+        self.addLink(R3, H3, cls=TCLink, bw=100, delay="1ms")
+
+        # subnet behind R5
+        H4 = self.addHost("H4", ip="10.2.0.2/24")
+        H5 = self.addHost("H5", ip="10.2.0.3/24")
+        self.addLink(R5, H4, cls=TCLink, bw=100, delay="0.05ms")
+        self.addLink(R5, H5, cls=TCLink, bw=100, delay="0.05ms")
+
+        # subnet behind R6
+        H6 = self.addHost("H6", ip="10.3.0.2/24")
+        self.addLink(R6, H6, cls=TCLink, bw=100, delay="1ms")
+
+        # subnet behind R2
+        PROXY = self.addHost("PROXY", ip="10.4.0.1/24")
+        S1 = self.addHost("S1", ip="10.4.0.2/24")
+        S2 = self.addHost("S2", ip="10.4.0.3/24")
+        self.addLink(R2, PROXY, cls=TCLink, bw=100, delay="1ms")
+        self.addLink(R2, S1, cls=TCLink, bw=100, delay="1ms")
+        self.addLink(R2, S2, cls=TCLink, bw=100, delay="1ms")
+
+
+def create_topology():
+    net = Mininet(topo=Topology())
+
+    CLI(net)
+    net.stop()
+
+
+if __name__ == "__main__":
+    create_topology()
